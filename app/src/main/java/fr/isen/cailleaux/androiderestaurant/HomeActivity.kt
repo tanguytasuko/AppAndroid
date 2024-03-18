559bde9a483942a044ad8e8dec3086bf2a0befcd
tanguytasuko <tanguy.cailleaux@isen.yncrea.fr>
2024-03-18 08:08:57 +0100

    Premier commit

---
+++ app/src/main/java/fr/isen/cailleaux/androiderestaurant/HomeActivity.kt
@@ -0,0 +1,47 @@
+package fr.isen.cailleaux.androiderestaurant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import fr.isen.cailleaux.androiderestaurant.ui.theme.AndroidERestaurantTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidERestaurantTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    HomeActivityScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeActivityScreen() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(text = "Entrée", style = MaterialTheme.typography.headlineLarge)
+        Text(text = "Plats", style = MaterialTheme.typography.headlineLarge)
+        Text(text = "Dessert", style = MaterialTheme.typography.headlineLarge)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidERestaurantTheme {
+        HomeActivityScreen()
+    }
+}
